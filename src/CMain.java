e129c91fdc39063dcca827288aecfc8f378d7cf9
ERotovsky <ERotovsky@CHE-D-230.rocketsoftware.com>
2012-07-19 11:36:56 +0600

    qwsq

--- src/CMain.java
+++ src/CMain.java
@@ -5,7 +5,7 @@ public class CMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hellsadcasdo World");
 		return;
 	}
 
